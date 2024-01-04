f8845aea12eae0af4432a836715f9e608946c841
AHsofty <e.basaran2004@gmail.com>
2024-01-04 14:08:11 +0100

    Feat: added an admin navbar

--- Client/Site/IRPWC-webshop/src/app/app-layout/app-layout.component.ts
+++ Client/Site/IRPWC-webshop/src/app/app-layout/app-layout.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import {AuthService} from "../shared/service/auth.service";
+import {NavbarAdminComponent} from "../navbar-admin/navbar-admin.component";
 
 @Component({
   selector: 'app-app-layout',
   standalone: true,
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, NavbarAdminComponent],
 })
 export class AppLayoutComponent {
+  public isAdmin: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isAdmin = this.authService.isAdmin();
+  }
 
 }
+
+
